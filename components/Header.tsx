06dd8ba964d84f2974fc23e2a00dac93bbdd6986
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-18 15:43:13 +0200

    feat: basic navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,44 @@
 import Link from "next/link";
 import CssLogo from "components/CssLogo";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+  const baseLinkClass = "text-gray-700 hover:underline hover:text-primary-500";
+  const activeLinkClass = (href: string) =>
+    router.asPath === href ? "underline" : "";
+  const links = [
+    {
+      href: "/",
+      label: "Home",
+    },
+    {
+      href: "/about",
+      label: "About",
+    },
+    {
+      href: "/resume",
+      label: "Resume",
+    },
+  ];
+
   return (
     <header className="container flex items-center justify-between p-4 mx-auto">
       <Link href="/" passHref={true}>
-        <CssLogo />
+        <a>
+          <CssLogo />
+        </a>
       </Link>
       <nav className="flex justify-end space-x-8">
-        <Link href="/" passHref={true}>
-          <span className="text-gray-700 link link-primary">Home</span>
-        </Link>
-        <Link href="/about" passHref={true}>
-          <span className="text-gray-700 link link-primary">About</span>
-        </Link>
-        <Link href="/resume" passHref={true}>
-          <span className="text-gray-700 link link-primary">Resume</span>
-        </Link>
+        {links.map(({ href, label }) => {
+          return (
+            <Link key={href} href={href} passHref={true}>
+              <a className={`${activeLinkClass(href)} ${baseLinkClass}`}>
+                {label}
+              </a>
+            </Link>
+          );
+        })}
       </nav>
     </header>
   );
