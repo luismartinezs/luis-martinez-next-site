ef8f44780aca54e255269107337e75d1690a4507
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-18 15:44:06 +0200

    feat: add basic footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-screen text-gray-600 bg-gray-200 body-font">
+      <div className="container flex flex-col items-center px-5 py-4 mx-auto sm:flex-row">
+        {new Date().getFullYear()} Luis Martinez
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
