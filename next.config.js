d0e7f9cd74ba9936848836e2aded846ed33c643b
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-18 22:09:36 +0200

    chore: add support for mdx

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
