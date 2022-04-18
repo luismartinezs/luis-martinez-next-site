12efbdfa6d18e99577268503a7b6f3a100eab9a7
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-18 15:48:22 +0200

    style: very minimalistic design

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,11 +7,17 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <>
-      <Header />
-      <main>{children}</main>
+    <div className="flex flex-col justify-between h-screen">
+      <div>
+        <div className="flex h-1">
+          <div className="flex-1 h-full bg-primary-500" />
+          <div className="flex-1 h-full bg-secondary-500" />
+        </div>
+        <Header />
+        <main className="w-full mx-auto mt-6">{children}</main>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
