5a952cdf0de7351ed188eb57b1b2a52699a0e3fb
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-21 11:27:51 +0200

    feat: responsive images

--- blocks/CloudinaryImage.tsx
+++ blocks/CloudinaryImage.tsx
@@ -1,26 +1,28 @@
 import { storyblokEditable } from "@storyblok/react";
 import BlockWrapper from "components/BlockWrapper";
-import Image from "next/image";
+// TODO having trouble with making image responsive with next/image!
+// import Image from "next/image";
 
 const CloudinaryImage = ({ blok }) => {
-  let { src, alt, width, height } = blok;
-  width = Number(width);
-  height = Number(height);
+  let { src, alt } = blok;
   const hostUrl = `https://res.cloudinary.com/dicyllvry/image/upload/q_100/luis-martinez`;
   return (
     <BlockWrapper width="text">
-      <div
-        {...storyblokEditable(blok)}
-        key={blok._uid}
-        style={{ position: "relative", width: "100%", height: "500px" }}
-      >
-        <Image
-          className="object-center object-cover w-full h-full"
+      <div {...storyblokEditable(blok)} key={blok._uid}>
+        <img
           src={`${hostUrl}/${src}`}
           alt={alt}
-          layout="fill"
-          objectFit="contain"
+          className="max-w-full h-auto"
         />
+        {/* <Image
+          src={`${hostUrl}/${src}`}
+          alt={alt}
+          width="100%"
+          height="100%"
+          layout="responsive"
+          objectFit="contain"
+          className="max-w-full h-auto"
+        /> */}
       </div>
     </BlockWrapper>
   );
