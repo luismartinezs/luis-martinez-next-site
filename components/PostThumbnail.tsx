4a98144c275b2cd7029a3257ee97248a054d2dc5
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-21 13:44:38 +0200

    style: post item on focus

--- components/PostThumbnail.tsx
+++ components/PostThumbnail.tsx
@@ -3,7 +3,7 @@ import CloudinaryImage from "components/CloudinaryImage";
 
 const PostThumbnail = ({ slug, title, image }) => {
   return (
-    <div className="border border-gray-200 rounded-xl shadow-lg overflow-hidden flex flex-col hover:scale-[1.02] transition ease-in-out hover:text-primary-500 text-gray-700 hover:border-primary-500 hover:shadow-xl">
+    <div className="border border-gray-200 rounded-xl shadow-lg overflow-hidden flex flex-col hover:scale-[1.02] transition ease-in-out hover:text-primary-500 text-gray-700 hover:border-primary-500 hover:shadow-xl focus-within:text-primary-500 focus-within:border-primary-500">
       <Link href={`/blog/${slug}`}>
         <a className="w-full h-full">
           <CloudinaryImage width={650} height={340} alt={title} src={image} />
