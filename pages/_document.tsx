839525849d5aa9d6935b90c83d4124a99ffbf3c6
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-22 10:24:17 +0200

    chore: copy gtm scripts from gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { GTM_ID } from 'lib/gtm'
+import { GTM_ID } from "lib/gtm";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
@@ -16,13 +16,13 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-        <noscript>
+          <noscript>
             <iframe
               src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
               height="0"
               width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
           </noscript>
           <Main />
           <NextScript />
