2ad1fcd4b064873b77f0d4ae1739a0d6ea35ad54
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-23 14:48:42 +0200

    chore: add netlify netxjs plugin

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -22,7 +22,7 @@ export async function getStaticProps({ params: { slug } }) {
     props: {
       post,
     },
-    revalidate: 10,
+    revalidate: 60,
   };
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export async function getStaticProps() {
     props: {
       posts,
     },
-    revalidate: 10,
+    revalidate: 60,
   };
 }
 
