bce352ba66c16f3d4da8840521ecd2a0a922bf64
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-23 09:37:55 +0200

    style: prose post title

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -34,7 +34,7 @@ export default function PostPage({ post }) {
         <meta name="description" content={post.content.description} />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <article className=" dark:prose-invert custom-prose">
+      <article className="prose dark:prose-invert custom-prose">
         <StoryblokComponent blok={post.content} />
       </article>
     </>
