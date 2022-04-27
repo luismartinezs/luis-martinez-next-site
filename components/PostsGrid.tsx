b2193209b7f736b56c19d740c355c1f203c19d54
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-27 14:22:41 +0200

    fix: dark mode title white

--- components/PostsGrid.tsx
+++ components/PostsGrid.tsx
@@ -11,7 +11,7 @@ const PostsGrid = ({ posts }) => {
   });
   return (
     <BlockWrapper>
-      <h2 className="text-3xl font-semibold text-gray-800">Blog posts</h2>
+      <h2 className="text-3xl font-semibold text-gray-800 dark:text-white">Blog posts</h2>
       <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mt-6 gap-4">
         {sortedPosts.map(({ slug, content: { postTitle, featuredImage } }) => (
           <PostThumbnail
