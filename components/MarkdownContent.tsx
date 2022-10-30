5ff7b35c5aae879471138550dd2f456ea3a8e4ef
Luis Martinez Suarez <luismartinezwebdev@gmail.com>
2022-10-30 21:32:41 +0100

    feat: markdown content component (#7)

--- components/MarkdownContent.tsx
+++ components/MarkdownContent.tsx
@@ -1,17 +1,18 @@
 import Head from "next/head";
-import md from "markdown-it";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const MarkdownContent = ({ frontmatter, content }) => {
   return (
     <>
-    <Head>
-      <title>{frontmatter.title}</title>
-    </Head>
-    <div className="prose dark:prose-invert custom-prose mx-auto">
-      <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
-    </div>
-  </>
-  )
-}
+      <Head>
+        <title>{frontmatter.title}</title>
+      </Head>
+      <div className="prose dark:prose-invert custom-prose mx-auto">
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      </div>
+    </>
+  );
+};
 
-export default MarkdownContent
+export default MarkdownContent;
