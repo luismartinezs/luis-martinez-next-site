bcc14dfdf3d44544aa33d44ea7f4a1b9c900a849
Luis Martinez <luismartinezwebdev@gmail.com>
2023-01-30 11:58:18 +0000

    fix: build sentry error

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,5 @@
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return <div className="mt-6 text-center">
+    <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+  </div>
 }
