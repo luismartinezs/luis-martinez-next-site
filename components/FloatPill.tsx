81ded3ea2d53f4d0ff465f9c3171a3e0edb6b638
Luis Martinez <luismartinezwebdev@gmail.com>
2023-02-28 11:29:46 +0000

    fix: a11y for pill

--- components/FloatPill.tsx
+++ components/FloatPill.tsx
@@ -2,7 +2,7 @@ import { type FC } from "react";
 
 const FloatPill: FC<{ label: string }> = ({ label }) => {
   return (
-    <div className="fixed bottom-4 right-6 z-10 rounded-full bg-white py-1 px-2 text-sm text-primary-800 shadow dark:bg-gray-700 dark:text-gray-300">
+    <div className="fixed bottom-4 right-6 z-10 rounded-full bg-white py-1 px-2 text-sm text-primary-800 shadow dark:bg-gray-700 dark:text-white">
       {label}
     </div>
   );
