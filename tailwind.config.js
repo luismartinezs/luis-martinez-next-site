015f7aab51fec225ac0e197f9f69ead9a1c4c144
Luis Martinez <luismartinezwebdev@gmail.com>
2024-07-17 17:45:28 +0700

    tw class mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  darkMode: "selector",
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
